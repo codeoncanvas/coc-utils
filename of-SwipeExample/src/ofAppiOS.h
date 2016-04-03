4bc18a4b10a6c319227cee653e479aa91a052e5c
lukasz karluk <julapy@googlemail.com>
2016-04-03 10:00:58 +1000

    SwipeExample working on iOS.

--- of-SwipeExample/src/ofAppiOS.h
+++ of-SwipeExample/src/ofAppiOS.h
@@ -1,26 +1,28 @@
 #pragma once
 
 #include "ofxiOS.h"
+#include "ofApp.h"
 
 class ofAppiOS : public ofxiOSApp{
-	
-    public:
-        void setup();
-        void update();
-        void draw();
-        void exit();
-	
-        void touchDown(ofTouchEventArgs & touch);
-        void touchMoved(ofTouchEventArgs & touch);
-        void touchUp(ofTouchEventArgs & touch);
-        void touchDoubleTap(ofTouchEventArgs & touch);
-        void touchCancelled(ofTouchEventArgs & touch);
+public:
 
-        void lostFocus();
-        void gotFocus();
-        void gotMemoryWarning();
-        void deviceOrientationChanged(int newOrientation);
+    void setup();
+    void update();
+    void draw();
+    void exit();
 
+    void touchDown(ofTouchEventArgs & touch);
+    void touchMoved(ofTouchEventArgs & touch);
+    void touchUp(ofTouchEventArgs & touch);
+    void touchDoubleTap(ofTouchEventArgs & touch);
+    void touchCancelled(ofTouchEventArgs & touch);
+
+    void lostFocus();
+    void gotFocus();
+    void gotMemoryWarning();
+    void deviceOrientationChanged(int newOrientation);
+
+    ofApp app;
 };
 
 
--- of-SwipeExample/src/ofAppiOS.mm
+++ of-SwipeExample/src/ofAppiOS.mm
@@ -2,37 +2,46 @@
 
 //--------------------------------------------------------------
 void ofAppiOS::setup(){
-
+    app.setup();
 }
 
 //--------------------------------------------------------------
 void ofAppiOS::update(){
-
+    app.update();
 }
 
 //--------------------------------------------------------------
 void ofAppiOS::draw(){
-	
+	app.draw();
 }
 
 //--------------------------------------------------------------
 void ofAppiOS::exit(){
-
+    app.exit();
 }
 
 //--------------------------------------------------------------
 void ofAppiOS::touchDown(ofTouchEventArgs & touch){
-
+    if(touch.id != 0) {
+        return;
+    }
+    app.mousePressed(touch.x, touch.y, touch.id);
 }
 
 //--------------------------------------------------------------
 void ofAppiOS::touchMoved(ofTouchEventArgs & touch){
-
+    if(touch.id != 0) {
+        return;
+    }
+    app.mouseDragged(touch.x, touch.y, touch.id);
 }
 
 //--------------------------------------------------------------
 void ofAppiOS::touchUp(ofTouchEventArgs & touch){
-
+    if(touch.id != 0) {
+        return;
+    }
+    app.mouseReleased(touch.x, touch.y, touch.id);
 }
 
 //--------------------------------------------------------------
