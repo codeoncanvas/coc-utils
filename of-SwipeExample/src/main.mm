db2f4e36dc9dd7bbf62c74d088ae5cd5602904e3
lukasz karluk <julapy@googlemail.com>
2016-04-03 10:07:17 +1000

    SwipeExample ios fixes.

--- of-SwipeExample/src/main.mm
+++ of-SwipeExample/src/main.mm
@@ -5,7 +5,7 @@ int main() {
     //  here are the most commonly used iOS window settings.
     //------------------------------------------------------
     ofiOSWindowSettings settings;
-    settings.enableRetina = true; // enables retina resolution if the device supports it.
+    settings.enableRetina = false; // enables retina resolution if the device supports it.
     settings.enableDepth = true; // enables depth buffer for 3d drawing.
     settings.enableAntiAliasing = true; // enables anti-aliasing which smooths out graphics on the screen.
     settings.numOfAntiAliasingSamples = 4; // number of samples used for anti-aliasing.
